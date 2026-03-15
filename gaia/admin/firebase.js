144f8ac04a37d56cd3dada2087c55e2e2153cef7
chaima belkhattabe <118306171+ChachaBel@users.noreply.github.com>
2026-03-15 14:13:00 +0100

    Add files via upload

--- gaia/admin/firebase.js
+++ gaia/admin/firebase.js
@@ -1,4 +1,7 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCNjSShwk3OTLgb5AZ4KhD3L1yDEMwDams",
   authDomain: "gaia-cosmeti.firebaseapp.com",
@@ -8,3 +11,7 @@ const firebaseConfig = {
   appId: "1:795239138362:web:87db3860228819400a6103",
   measurementId: "G-ZMBDK3NWTJ"
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
